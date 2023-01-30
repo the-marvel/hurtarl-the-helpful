27e569e83cb5382ab175841c760e998447a335b9
doug <douglas.j.marvel@gmail.com>
2023-01-30 18:59:23 -0500

    config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `hurtarlthehelpfulmain.gatsbyjs.io`,
+    siteUrl: `http://hurtarlthehelpfulmain.gatsbyjs.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
