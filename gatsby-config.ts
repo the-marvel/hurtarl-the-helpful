aa27c079cb2a89a1eb691bfe6f7da5687b6fc661
doug <douglas.j.marvel@gmail.com>
2023-01-30 18:53:35 -0500

    data and config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,20 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `hurtarlthehelpfulmain.gatsbyjs.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`}
+    }
+  ],
 }
 
 export default config
